6cde33e84d5551993f1ac1e4a81bb29ecfd93a39
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2026-05-19 15:38:55 +0200

    removed the color token details from the page

--- src/theme-overview-element.ts
+++ src/theme-overview-element.ts
@@ -79,6 +79,7 @@ export class ThemeOverview extends LitElement {
     );
 
     console.log({ tokens: this.json, evilTokens, niceTokens });
+    console.log( niceColors )
 
     return html`<h2>tokens</h2>
       <ul>
@@ -98,27 +99,11 @@ export class ThemeOverview extends LitElement {
             </li>`,
         )}
       </ul>
-      <h2>nice tokens ❤️</h2>
+      <h2>Colors 🌈</h2>
       <ul>
         ${niceColors.length === 0 
             ? html`<li style="color: black">Fout: Er worden geen kleuren uit het NL Design System gebruikt.</li>` 
             : html `<li style="color: black"> Goed: ${niceColors.length} kleur tokens gevonden! </li>`}
-                    ${niceTokens.map(
-          (token) =>
-            html`<li
-              style="color: ${token["$extensions"][
-                "nl.nldesignsystem.theme-wizard.css-authored-as"
-              ]}"
-            >
-              token:
-              <code
-                >${token["$extensions"][
-                  "nl.nldesignsystem.theme-wizard.css-authored-as"
-                ]}</code
-              >
-            </li>`,
-        )}
-
       </ul>
       <h2>evil tokens!!!</h2>
       <ul>
